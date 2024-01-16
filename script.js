c9e24f53f0e3cb57738e56abc9c48d22b1b02aad
Mohammed Rahman <mohammed.rahman676@gmail.com>
2024-01-16 16:26:33 +0000

    linked search-input to button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const apiKey = 'cd8e5e5c2b38a09b7e5141e82668051f';
 const FCApiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
 
+// Fetch Data for London
+
 fetch(FCApiURL)
   .then(function (response) {
     console.log('resolved', response);
@@ -12,17 +14,34 @@ fetch(FCApiURL)
     console.log('rejected', err);   
   });
 
+
+  // Functionality to Click Search Button
+
   $(document).ready(function() {
     $('#search-button').click(function(event) {
         event.preventDefault();
-        console.log('button click works');
+        const userInput = $('#search-input').val();
+        console.log(userInput);
     });
 });
+
+  // // Functionality to get Current London Weather Data
+  // $(document).ready(function() {
+
+  //   // Create City
+  //   const london = $('<div></div>');
+  //   london.text('London');
+  //   $('#today').append(London);
+
+  //   document.getElementById('.today') = data.name;
+  //   document.getElementById('.today') = data.main.temp;
+  //   document.getElementById('.today') = data.wind;
+  //   document.getElementById('.today') = data.main.humidity;
+    
+  // });
+
 
 
   
 
-   // document.getElementById('.today') = data.name;
-    // document.getElementById('.today') = data.main.temp;
-    // document.getElementById('.today') = data.wind;
-    // document.getElementById('.today') = data.main.humidity;
+   
