8fe621b6f0fd42151ea361d39af4b2bd7a550b4e
Mohammed Rahman <mohammed.rahman676@gmail.com>
2024-01-17 14:25:21 +0000

    created text for london including linking data from API, adding current ddate

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const apiKey = 'cd8e5e5c2b38a09b7e5141e82668051f';
-const FCApiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+const ApiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
 
 // Fetch Data for London
-
-fetch(FCApiURL)
+fetch(ApiURL)
   .then(function (response) {
     console.log('resolved', response);
     return response.json();
   }).then(function (data) {
     console.log(data);
+
+    // Call the selectCity function with the fetched data
+    selectCity(data);
   }).catch(function (err) { 
     console.log('rejected', err);   
   });
@@ -21,24 +23,39 @@ fetch(FCApiURL)
     $('#search-button').click(function(event) {
         event.preventDefault();
         const userInput = $('#search-input').val();
-        console.log(userInput);
+        fetch(selectCity(data));
     });
 });
 
+
   // // Functionality to get Current London Weather Data
-  // $(document).ready(function() {
+  function selectCity(data) {
+    // Create City elements
+    const cityName = $('<h2>');
+    const cityTemp = $('<div>');
+    const cityWind = $('<div>');
+    const cityHumid = $('<div>');
 
-  //   // Create City
-  //   const london = $('<div></div>');
-  //   london.text('London');
-  //   $('#today').append(London);
+    cityName.text(data.name + ' - ' + dayjs().format('DD/MM/YYYY'));
+    cityTemp.text('Temperature: ' + data.main.temp + ' °C');
+    cityWind.text('Wind: ' + data.wind.speed + ' KPH');
+    cityHumid.text('Humidity: ' + data.main.humidity + '%');
 
-  //   document.getElementById('.today') = data.name;
-  //   document.getElementById('.today') = data.main.temp;
-  //   document.getElementById('.today') = data.wind;
-  //   document.getElementById('.today') = data.main.humidity;
+    // Append elements to the DOM
+    $('#today').append(cityName);
+    $('#today').append(cityTemp);
+    $('#today').append(cityWind);
+    $('#today').append(cityHumid);
+
+    cityName.after('<br>');
+    cityTemp.after('<br>');
+    cityWind.after('<br>');
+}
+
+
+   
     
-  // });
+ 
 
 
 
